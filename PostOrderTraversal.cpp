b9d16d223a10113a0e0e2df9b891ba1457573056
Manish Kumar <manish.hit2800@gmail.com>
2021-05-22 20:29:09 +0530

    PostOrderTraversal

---
+++ PostOrderTraversal.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+     char data;
+     Node*left;
+     Node*right;
+};
+Node* root = NULL;
+struct Node* insert(struct Node* root, char x) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node*));
+    temp->data = x;
+    temp->left = NULL;
+    temp->right = NULL;
+    if(root == NULL) {
+        root = temp;
+    }
+    else if(x <= root->data) {
+        root->left = insert(root->left, x);
+    } else {
+        root->right = insert(root->right, x);
+    }
+    return root;
+}
+
+void PostOrder (Node* root) {
+    if (root == NULL) {
+        return;
+    }
+    PostOrder(root->left);
+    PostOrder(root->right);
+    cout<<root->data<<" ";
+}
+
+int main() {
+    root = insert(root,'F');
+    root = insert(root,'D');
+    root = insert(root,'J');
+    root = insert(root,'B');
+    root = insert(root,'E');
+    root = insert(root,'A');
+    root = insert(root,'C');
+    root = insert(root,'G');
+    root = insert(root,'K');
+    root = insert(root,'I');
+    root = insert(root,'H');
+    PostOrder(root);
+}
