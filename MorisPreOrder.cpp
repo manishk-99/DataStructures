8eaae20493766f77f36eb2d3d44df7eb06b22224
Manish Kumar <manish.hit2800@gmail.com>
2021-06-06 23:39:53 +0530

    MorisPreOrder

---
+++ MorisPreOrder.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+     int data;
+     Node*left;
+     Node*right;
+};
+Node* root = NULL;
+struct Node* insert(struct Node* root, int x) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node*));
+    temp->data = x;
+    temp->left = NULL;
+    temp->right = NULL;
+    if(root == NULL) {
+        root = temp;
+    }
+    else if(x <= root->data) {
+        root->left = insert(root->left, x);
+    } else {
+        root->right = insert(root->right, x);
+    }
+    return root;
+}
+
+void MorrisPreOrder (Node* root) {
+    struct Node* current = root;
+    while(current != NULL) {
+        if(current->left == NULL) {
+            cout<<current->data<<endl;
+            current =  current->right;
+        } else {
+            struct Node* predecessor = current->left;
+            while(predecessor->right!=current && predecessor->right!=NULL) {
+                predecessor = predecessor->right;
+            }
+            if(predecessor->right == NULL) {
+                predecessor->right = current;
+                cout<<current->data<<endl;
+                current=current->left;
+            } else {
+                predecessor->right = NULL;
+                current = current->right;
+            }
+        }
+    }
+}
+
+int main() {
+    root = insert(root,10);
+    root = insert(root,5);
+    root = insert(root,30);
+    root = insert(root,-2);
+    root = insert(root,6);
+    root = insert(root,2);
+    root = insert(root,-1);
+    root = insert(root,8);
+    root = insert(root,40);
+    MorrisPreOrder(root);
+}
